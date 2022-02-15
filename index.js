5714fe1fefaee60e2e4799582dd2957fd0b19e15
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-15 16:34:18 -0300

    imp: req 1 endpoint GET /talker

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const controllers = require('./controllers');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', controllers.listTalkers);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
