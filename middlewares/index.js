5d06beecb54f818ece976a414fb22b4ef2ecf897
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-16 18:47:55 -0300

    updt: middlewares index

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,16 @@ const errorHandler = require('./errorHandler');
 const validateToken = require('./validateToken');
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validateName = require('./validateName');
+const validateAge = require('./validateAge');
+const validateTalk = require('./validateTalk');
 
 module.exports = { 
   errorHandler,
   validateToken,
   validateEmail,
-  validatePassword, 
+  validatePassword,
+  validateName,
+  validateAge,
+  validateTalk,
 };
