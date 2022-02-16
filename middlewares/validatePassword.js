d0f7bf3de90b16ab48f58d0fcf9e90d38dc2f96b
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-16 10:52:25 -0300

    imp: middleware  validate password

---
+++ middlewares/validatePassword.js
@@ -0,0 +1,15 @@
+const validatePassword = (req, res, next) => {
+  try {
+    const { password } = req.body;
+
+    if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = validatePassword;
