6b9bae497f5a58903df3c6ddd6d626c2212d70b9
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-16 18:47:01 -0300

    updt: controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const listTalkers = require('./listTalkers');
 const listTalkerId = require('./listTalkerId');
 const createLogin = require('./createLogin');
+const createTalker = require('./createTalker');
 
 module.exports = { 
   listTalkers, 
   listTalkerId, 
   createLogin,
+  createTalker,
 };
