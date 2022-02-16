65b7bad9b545471faca68dcea6f410b666be9fa8
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-16 11:08:00 -0300

    imp: req 3 endpoint POST '/login'

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const middleware = require('./middlewares');
 const controllers = require('./controllers');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(middleware.errorHandler);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validationLogin = [
+  middleware.validateEmail,
+  middleware.validatePassword,
+];
+
 app.get('/talker', controllers.listTalkers);
 app.get('/talker/:id', controllers.listTalkerId);
+
+app.post(
+  '/login', 
+  validationLogin,
+  controllers.createLogin,
+);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
