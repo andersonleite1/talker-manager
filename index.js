158d770063dfd951644d19fde20846418938d621
Anderson Leite <andersonleite.dev@gmail.com>
2022-02-18 17:28:05 -0300

    updt: index add new port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(middleware.errorHandler);
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = process.env.PORT || '3000';
 
 app.use('/login', loginRouter);
 app.use('/talker', talkerRouter);
